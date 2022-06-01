766bb3b8160a076e6169f34c9770b53e388089c5
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-06-01 14:45:34 -0500

    create items file

---
+++ assessment/src/items.js
@@ -0,0 +1,16 @@
+const items = [
+    {
+      label: "Meets Expectations",
+      value: "meets-expectations"
+    },
+    {
+      label: "Partially Meets Expectations",
+      value: "partially-meets-expectations"
+    },
+    {
+      label: "Does Not Meet Expectations",
+      value: "does-not-meet-expectations"
+    }
+];
+
+  export default items
